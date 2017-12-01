f813d60fe8a20399549be2e02fb62d2eb57c860a
YuhangJasonLiu <18810815810@163.com>
2017-12-01 14:38:19 +0000

    Sync with Local modifications
    Remove some of unused classes
    Modifications on TopicModel.java

--- mallet-2.0.8/src/cc/mallet/examples/TopicModel.java
+++ mallet-2.0.8/src/cc/mallet/examples/TopicModel.java
@@ -1,16 +1,12 @@
 package cc.mallet.examples;
 
-import cc.mallet.util.*;
 import cc.mallet.types.*;
 import cc.mallet.pipe.*;
 import cc.mallet.pipe.iterator.*;
 import cc.mallet.topics.*;
 
 import java.util.*;
-import java.util.regex.*;
 import java.io.*;
-
-import java.util.Scanner;
 
 public class TopicModel {
 
@@ -18,18 +14,21 @@ public class TopicModel {
 		String fileName;
 		int topicNumbers;
 		Scanner sc = new Scanner(System.in);
+		File file;
 		while (true) {
 			System.out.println("Enter the filename for topic modeling:");
 			fileName = sc.nextLine();
-			File file = new File(fileName);
+			file = new File(fileName);
 			if (file.exists()) {
 				break;
 			} else {
 				System.out.println("File not found");
 			}
 		}
+
 		System.out.println("Enter topic number:");
 		topicNumbers = sc.nextInt();
+		sc.close();
 
 		// Begin by importing documents from text to feature sequences
 		ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
@@ -37,19 +36,12 @@ public class TopicModel {
 		// Pipes: lowercase, tokenize, remove stopwords, map to features
 		pipeList.add(new CharSequenceLowercase());
 		pipeList.add(new CharSequence2TokenSequence());
-		pipeList.add(new TokenSequenceRemoveStopwords(new File("stoplist.txt"), "UTF-8", false, false, false));
+		pipeList.add(new TokenSequenceRemoveStopwords(new File("stoplist.txt"), "UTF-8", true, false, true));
 		pipeList.add(new TokenSequence2FeatureSequence());
 
 		InstanceList instances = new InstanceList(new SerialPipes(pipeList));
-		try {
-			Reader fileReader = new InputStreamReader(new FileInputStream(new File(fileName)), "UTF-8");
-			instances.addThruPipe(new SelectiveFileLineIterator(fileReader, "^\\s*#.+")); // data,
-																							// label,
-																							// name
-																							// fields
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
-		}
+		Reader fileReader = new InputStreamReader(new FileInputStream(new File(fileName)), "UTF-8");
+		instances.addThruPipe(new SelectiveFileLineIterator(fileReader, "^\\s*#.+")); // data,
 
 		// Create a model with 100 topics, alpha_t = 0.01, beta_w = 0.01
 		// Note that the first parameter is passed as the sum over topics, while
@@ -62,7 +54,7 @@ public class TopicModel {
 		// Use two parallel samplers, which each look at one half the corpus and
 		// combine
 		// statistics after every iteration.
-		model.setNumThreads(4);
+		model.setNumThreads(2);
 
 		// Run the model for 50 iterations and stop (this is for testing only,
 		// for real applications, use 1000 to 2000 iterations)
@@ -78,11 +70,14 @@ public class TopicModel {
 		LabelSequence topics = model.getData().get(0).topicSequence;
 
 		Formatter out = new Formatter(new StringBuilder(), Locale.US);
+		Formatter out2 = new Formatter(new StringBuilder(), Locale.US);
+
 		for (int position = 0; position < tokens.getLength(); position++) {
 			out.format("%s-%d ", dataAlphabet.lookupObject(tokens.getIndexAtPosition(position)),
 					topics.getIndexAtPosition(position));
 		}
-		System.out.println(out);
+
+		// System.out.println(out);
 
 		// Estimate the topic distribution of the first instance,
 		// given the current Gibbs state.
@@ -90,41 +85,68 @@ public class TopicModel {
 
 		// Get an array of sorted sets of word ID/count pairs
 		ArrayList<TreeSet<IDSorter>> topicSortedWords = model.getSortedWords();
+		// Get an array of documents in each Topic
+		ArrayList<TreeSet<IDSorter>> topicDocuments = model.getTopicDocuments(numTopics);
 
 		// Show top 5 words in topics with proportions for the first document
 		for (int topic = 0; topic < numTopics; topic++) {
-			Iterator<IDSorter> iterator = topicSortedWords.get(topic).iterator();
+			Iterator<IDSorter> wordIterator = topicSortedWords.get(topic).iterator();
 
 			out = new Formatter(new StringBuilder(), Locale.US);
+
 			out.format("%d\t%.3f\t", topic, topicDistribution[topic]);
 			int rank = 0;
-			while (iterator.hasNext() && rank < 5) {
-				IDSorter idCountPair = iterator.next();
+			while (wordIterator.hasNext() && rank < 5) {
+				IDSorter idCountPair = wordIterator.next();
 				out.format("%s (%.0f) ", dataAlphabet.lookupObject(idCountPair.getID()), idCountPair.getWeight());
 				rank++;
 			}
+
 			System.out.println(out);
-		}
+//			 try {
+//			 // Open a file writer, the sencond parameter was set as "true"
+//			 // to append to file
+//			File writeFile = new File("testWrite.txt");
+//			 FileWriter writer = new FileWriter("testWrite.txt", true);
+//			 PrintWriter printWriter = new PrintWriter(writeFile);
+//			 model.printTopicDocuments(printWriter);
+//			 writer.close();
+//			 } catch (IOException e) {
+//			 e.printStackTrace();
+//			 }
+//			out.close();
+//
+//		}
 
 		// Create a new instance with high probability of topic 0
-		StringBuilder topicZeroText = new StringBuilder();
-		Iterator<IDSorter> iterator = topicSortedWords.get(0).iterator();
+		// StringBuilder topicZeroText = new StringBuilder();
+		// Iterator<IDSorter> iterator = topicSortedWords.get(0).iterator();
+		//
+		// int rank = 0;
+		// while (iterator.hasNext() && rank < 5) {
+		// IDSorter idCountPair = iterator.next();
+		// topicZeroText.append(dataAlphabet.lookupObject(idCountPair.getID()) +
+		// " ");
+		// rank++;
+		// }
 
-		int rank = 0;
-		while (iterator.hasNext() && rank < 5) {
-			IDSorter idCountPair = iterator.next();
-			topicZeroText.append(dataAlphabet.lookupObject(idCountPair.getID()) + " ");
-			rank++;
-		}
-
-		// Create a new instance named "test instance" with empty target and
-		// source fields.
-		InstanceList testing = new InstanceList(instances.getPipe());
-		testing.addThruPipe(new Instance(topicZeroText.toString(), null, "test instance", null));
-
-		TopicInferencer inferencer = model.getInferencer();
-		double[] testProbabilities = inferencer.getSampledDistribution(testing.get(0), 10, 1, 5);
-		System.out.println("0\t" + testProbabilities[0]);
 	}
+		
+		for (int topic = 0; topic < numTopics; topic++) {
+			Iterator<IDSorter> docIterator = topicDocuments.get(topic).iterator();
 
+			out = new Formatter(new StringBuilder(), Locale.US);
+
+			out.format("%d\t%.3f\t", topic, topicDistribution[topic]);
+			int rank = 0;
+			while (docIterator.hasNext() && rank < 5) {
+				IDSorter idCountPair = docIterator.next();
+				out.format("%s (%.0f) ", dataAlphabet.lookupObject(idCountPair.getID()), idCountPair.getWeight());
+				rank++;
+			}
+
+			System.out.println(out);
+			out.close();
+		}
+}
 }
